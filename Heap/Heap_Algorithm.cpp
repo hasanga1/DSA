47626acba836470e5f3b9f5ebf7dd2f3f8388d07
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2023-06-02 17:11:12 +0530

    Updated

--- Heap/Heap_Algorithm.cpp
+++ Heap/Heap_Algorithm.cpp
@@ -11,8 +11,74 @@ class Heap {
 private:
     vector<T> heap;
     bool isBuildHeap = false;
+    long size;
 
     void maxHeapify(long currentIndex) {
+        long left = (currentIndex + 1) * 2 - 1;
+        long right = (currentIndex + 1) * 2;
+        long largest = currentIndex;
+        if (left < size && heap[left] > heap[currentIndex]) {
+            largest = left;
+        }
+        if (right < size && heap[right] > heap[largest]) {
+            largest = right;
+        }
+        if (largest != currentIndex) {
+            T temp = heap[currentIndex];
+            heap[currentIndex] = heap[largest];
+            heap[largest] = temp;
+            maxHeapify(largest);
+        }
+    }
+
+//    void maxHeapify(long currentIndex) {
+//        while (heap[currentIndex] > heap[(currentIndex + 1) / 2 - 1] && currentIndex > 0)  {
+//            T element = heap[currentIndex];
+//            heap[currentIndex] = heap[(currentIndex + 1) / 2 - 1];
+//            heap[(currentIndex + 1) / 2 - 1] = element;
+//            currentIndex = (currentIndex + 1) / 2 - 1;
+//        }
+//    }
+
+//    void minHeapify(long currentIndex) {
+//        while (heap[currentIndex] < heap[(currentIndex + 1) / 2 - 1] && currentIndex > 0)  {
+//            T element = heap[currentIndex];
+//            heap[currentIndex] = heap[(currentIndex + 1) / 2 - 1];
+//            heap[(currentIndex + 1) / 2 - 1] = element;
+//            currentIndex = (currentIndex + 1) / 2 - 1;
+//        }
+//    }
+
+    void buildHeap() {
+        isBuildHeap = true;
+        for (int i = size / 2 - 1; i > 0; i--) {
+            maxHeapify(i);
+        }
+    }
+
+public:
+    Heap(T root) {
+        heap.push_back(root);
+        size++;
+    }
+
+    Heap() {}
+
+    Heap(vector<T> arr) {
+        this->heap = arr;
+        this->size = arr.size();
+    }
+
+    void insert(T element) {
+        size++;
+        isBuildHeap = false;
+        if (heap.size() == 0) {
+            heap.push_back(element);
+            return;
+        }
+        heap.push_back(element);
+        int currentIndex = heap.size() - 1;
+
         while (heap[currentIndex] > heap[(currentIndex + 1) / 2 - 1] && currentIndex > 0)  {
             T element = heap[currentIndex];
             heap[currentIndex] = heap[(currentIndex + 1) / 2 - 1];
@@ -21,42 +87,15 @@ private:
         }
     }
 
-    void minHeapify(long currentIndex) {
-        while (heap[currentIndex] < heap[(currentIndex + 1) / 2 - 1] && currentIndex > 0)  {
-            T element = heap[currentIndex];
-            heap[currentIndex] = heap[(currentIndex + 1) / 2 - 1];
-            heap[(currentIndex + 1) / 2 - 1] = element;
-            currentIndex = (currentIndex + 1) / 2 - 1;
-        }
-    }
+    T pop_front() {
+        T temp = heap[0];
+        heap[0] = heap[heap.size() - 1];
+        heap.pop_back();
+        size--;
 
-    void buildHeap() {
-        isBuildHeap = true;
-        for (int i = heap.size() / 2 - 1; i > 0; i--) {
-            maxHeapify(i);
-        }
-    }
+        maxHeapify(0);
 
-public:
-    Heap(T root) {
-        heap.push_back(root);
-    }
-
-    Heap() {}
-
-    Heap(vector<T> arr) {
-        this->heap = arr;
-    }
-
-    void insert(T element) {
-        isBuildHeap = false;
-        if (heap.size() == 0) {
-            heap.push_back(element);
-            return;
-        }
-        heap.push_back(element);
-        int currentIndex = heap.size() - 1;
-        maxHeapify(currentIndex);
+        return temp;
     }
 
     void clear() {
@@ -83,10 +122,10 @@ public:
             T temp = heap[i];
             heap[i] = heap[0];
             heap[0] = temp;
-
+            size--;
             maxHeapify(0);
-            this->print();
         }
+        size = heap.size();
         return heap;
     }
 };
@@ -98,6 +137,9 @@ int main() {
     heap->insert(8);
     heap->insert(10);
     heap->insert(20);
+    heap->insert(18);
+    heap->print();
+    heap->pop_front();
     heap->print();
 
     vector<int> arr = heap->heapSort();
